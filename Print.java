818772b6b5f28cd2df21e2e2f8b3bb9deeca054e
Niraj Pandit <panditniraj320@gmail.com>
2024-06-27 14:04:37 +0545

    latest commit

--- Print.java
+++ Print.java
@@ -2,6 +2,9 @@ class Print{
     public static void main(String a[]) {
         System.out.println(5+3);
         System.out.println(5+9);
-
+        System.err.println("Latest");
     }
 }
+// git add .
+// git commit -m "message"
+// git push
