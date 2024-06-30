34a93cad39e20745ae0c7ca5f1cf0b695f573874
Niraj Pandit <panditniraj320@gmail.com>
2024-06-30 19:54:14 +0545

    Diving two numbers

---
+++ Division.java
@@ -0,0 +1,12 @@
+public class Division {
+    public static void main(String[] args) {
+        int Div1=20;      // Declaring variables
+        int Div2 =10;
+
+        int Final_Answer= (Div1 / Div2);
+
+        System.out.println("The sum of " + Div1 + " and " + Div2 + " is: " + Final_Answer);  // Adding two numbers
+
+      
+    }
+}
