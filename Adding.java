cb666e9eb4df998d9ac7dbe86ca4967f26b687fc
Niraj Pandit <panditniraj320@gmail.com>
2024-06-30 19:38:58 +0545

    Adding two numbers

---
+++ Adding.java
@@ -0,0 +1,12 @@
+public class Adding {
+    public static void main(String[] args) {
+        int num1 =10;      // Declaring variables
+        int num2 =19;
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);  // Adding two numbers
+
+      
+    }
+}
