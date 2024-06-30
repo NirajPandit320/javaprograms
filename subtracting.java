06ec41eb5367d56985ae57aed654ab85555e8e58
Niraj Pandit <panditniraj320@gmail.com>
2024-06-30 19:49:26 +0545

    Subtracting Numbers

---
+++ subtracting.java
@@ -0,0 +1,12 @@
+public class subtracting{
+    public static void main(String args[]) {
+        int num1 =20;      // Declaring variables
+        int num2 =19;
+
+        int subtraction = (num1 -num2);
+
+        System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + subtraction);  // Subtracting two numbers
+
+      
+    }
+}
