85536d2a5c3c5a0a78355f1baacbe7b65e6e801d
Niraj Pandit <panditniraj320@gmail.com>
2024-09-06 01:08:03 +0530

    Creating Object

---
+++ newobj.java
@@ -0,0 +1,18 @@
+class object
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Thids is main method obviously");
+
+        //Now creting the object of the newobj
+        newobj sc = new newobj();
+        sc.dis();
+    }
+}
+public class newobj
+{
+    void dis()
+    {
+        System.out.println("This is temporary method for recalling");
+    }
+}
---
+++ object.java
@@ -0,0 +1,22 @@
+class object
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is main Method");
+
+        over sc=new over();
+        sc.add();
+    }
+    
+}
+
+
+
+class over
+{
+    void add()
+    {
+        System.out.println("This is Temporary");
+        
+    }
+}
